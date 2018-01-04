39a48d2a8b2c0275c2d610ef4f4984a233b56613
Manikanta <Manikantagv279@gmail.com>
2018-01-04 12:09:12 +0530

    Multiplication fn done

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ def add(x,y,z):
     return (x+y+z)
 def subtract(x,y,z):
     return (x-y-z)
-def multiply(x,y):
-    return (x*y)
+def multiply(x,y,z):
+    return (x*y*z)
 def divide(x,y):
     return (x/y)
